98f4455e3ce8c4b7158d1df5def8c47203143bf0
felix arnet <farnet@gmx.net>
2017-03-25 20:04:14 +0100

    OcadScratch

---
+++ OcadScratch/Data.Wpf.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Media;
+
+namespace OMapScratch
+{
+  public partial class Map
+  {
+    private System.Collections.Generic.List<ColorRef> GetDefaultColors()
+    {
+      return new System.Collections.Generic.List<ColorRef>
+      {
+        new ColorRef { Id = "Bl", Color = Colors.Black },
+        new ColorRef { Id = "Gy", Color = Colors.Gray },
+        new ColorRef { Id = "Bw", Color = Colors.Brown },
+        new ColorRef { Id = "Y", Color = Colors.Yellow },
+        new ColorRef { Id = "G", Color = Colors.Green },
+        new ColorRef { Id = "K", Color = Colors.Khaki },
+        new ColorRef { Id = "R", Color = Colors.Red },
+        new ColorRef { Id = "B", Color = Colors.Blue } };
+    }
+  }
+  public partial class ColorRef
+  {
+    public Color Color { get; set; }
+  }
+  public partial class XmlColor
+  {
+    private void GetEnvColor(ColorRef color)
+    {
+      color.Color = new Color { R = Red, G = Green, B = Blue };
+    }
+
+    private void SetEnvColor(ColorRef color)
+    {
+      Red = color.Color.R;
+      Green = color.Color.G;
+      Blue = color.Color.B;
+    }
+  }
+}
---
+++ OcadScratch/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using OMapScratch;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace OcadScratch
+{
+  /// <summary>
+  /// Interaktionslogik für MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnSymbols_Click(object sender, RoutedEventArgs e)
+    {
+      Map map = new Map();
+      StringBuilder sb = new StringBuilder();
+      using (var w = new StringWriter(sb))
+      {
+        XmlSymbols syms = XmlSymbols.Create(map.GetSymbols());
+        syms.Colors = new List<XmlColor>();
+        foreach (ColorRef color in map.GetColors())
+        { syms.Colors.Add(XmlColor.Create(color)); }
+        Serializer.Serialize(syms, w);
+      }
+
+      string first = sb.ToString();
+      XmlSymbols des;
+      using (var r = new StringReader(first))
+      {
+        Serializer.Deserialize(out des, r);
+      }
+
+      List<Symbol> symbols = new List<Symbol>();
+      foreach (XmlSymbol s in des.Symbols)
+      { symbols.Add(s.GetSymbol()); }
+      sb = new StringBuilder();
+      using (var w = new StringWriter(sb))
+      {
+        Serializer.Serialize(XmlSymbols.Create(symbols), w);
+      }
+      if (sb.ToString() != first)
+      { throw new InvalidDataException(""); }
+    }
+  }
+}
